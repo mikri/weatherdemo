30f3739e0bd1a7c4739ef8eb9e7a763ae7e76109
Mikko Hyytiäinen <mikko.hyytiainen@gmail.com>
2015-12-07 19:57:04 +0200

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name weatherappApp
+ * @description
+ * # weatherappApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('weatherappApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'uiGmapgoogle-maps'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+  .config(function(uiGmapGoogleMapApiProvider) {
+      uiGmapGoogleMapApiProvider.configure({
+          //    key: 'your api key',
+          v: '3.20',
+          libraries: 'weather,geometry,visualization'
+      });
+  });
